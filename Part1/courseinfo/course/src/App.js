ab6629c316da76708dff4d6271992d31a8ddbde7
KartanonKanye <rruuskanen12@gmail.com>
2023-02-21 12:35:10 +0200

    Refactored App.js by adding three new components.

--- Part1/courseinfo/course/src/App.js
+++ Part1/courseinfo/course/src/App.js
@@ -1,25 +1,37 @@
+const Header = (props) => {
+  return(
+    <h1>{props.course_name}</h1>
+  )
+}
+
+const Content = (props) => {
+  return(
+    <p>{props.part_name} {props.part_count} </p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p> Number of exercises {props.sum} </p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
-  const exercises2 = 7
+  const exercises2 = 9
   const part3 = 'State of a component'
   const exercises3 = 14
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      < Header course_name= {course}/>
+      < Content part_name = {part1} part_count = {exercises1} />
+      < Content part_name = {part2} part_count = {exercises2} />
+      < Content part_name = {part3} part_count = {exercises3} />
+      < Total sum = {exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
